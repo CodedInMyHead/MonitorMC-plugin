632d3e6519a18159dd5f9e20f7b34e27ccd3bca5
Ole <ole.lorbacher@gmail.com>
2023-06-15 18:53:54 +0200

    initial commit

---
+++ src/main/java/com/codedinmyhead/monitormc/monitormc/MonitorMC.java
@@ -0,0 +1,18 @@
+package com.codedinmyhead.monitormc.monitormc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MonitorMC extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        System.out.println("Start");
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
