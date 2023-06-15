0835dbd1c7511063a8e0ff7a291d7f64037351f3
Ole <ole.lorbacher@gmail.com>
2023-06-15 19:03:24 +0200

    test logging

--- src/main/java/com/codedinmyhead/monitormc/monitormc/MonitorMC.java
+++ src/main/java/com/codedinmyhead/monitormc/monitormc/MonitorMC.java
@@ -2,17 +2,22 @@ package com.codedinmyhead.monitormc.monitormc;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Logger;
+
 public final class MonitorMC extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-        System.out.println("Start");
+        Logger logger = this.getLogger();
+        logger.info("STARTUP");
 
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        Logger logger = this.getLogger();
+        logger.info("SHUTDOWN");
     }
 }
