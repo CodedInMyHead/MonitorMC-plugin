0cbaa1dd69b3583f104faf94f1c0bd11536f9f8c
I574453 <tristan.sievertsen@sap.com>
2023-07-20 09:28:13 +0200

    Umverlegung der Logik aus MonitorMC nach PlayerpathCommand

--- src/main/java/com/codedinmyhead/monitormc/monitormc/commands/PlayerpathCommand.java
+++ src/main/java/com/codedinmyhead/monitormc/monitormc/commands/PlayerpathCommand.java
@@ -6,15 +6,15 @@ import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
+import org.dynmap.markers.Marker;
+import org.dynmap.markers.MarkerSet;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
-public class PlayerpathCommand implements CommandExecutor {
+public class PlayerpathCommand implements CommandExecutor, TabCompleter {
 
     //Speicherung der playerpath-Koordinaten:
     //ArrayList enthält 3 ArrayLists für koordinaten -> je eine für x, y, z
@@ -23,8 +23,8 @@ public class PlayerpathCommand implements CommandExecutor {
 
 
     @Override
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args){
-        if (sender instanceof Player){
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (sender instanceof Player) {
             Player p = (Player) sender;
             Location pLoc = p.getLocation();
             String pUUID = p.getUniqueId().toString();
@@ -33,18 +33,17 @@ public class PlayerpathCommand implements CommandExecutor {
             addCoordinatesToPlayerpath(pLoc, pUUID);
 
 
-
             sender.sendMessage("Position gespeichert. Argumente: " + args[0]);
         }
 
         return false;
     }
 
-    private void addCoordinatesToPlayerpath(Location pLoc, String pUUID){
+    private void addCoordinatesToPlayerpath(Location pLoc, String pUUID) {
         ArrayList<Location> pPath = new ArrayList<>();
         String pName = Bukkit.getPlayer(UUID.fromString(pUUID)).getName();
 
-        if (collectionPlayerpaths.containsKey(pUUID)){
+        if (collectionPlayerpaths.containsKey(pUUID)) {
             pPath = collectionPlayerpaths.get(pUUID);
             pPath.add(pLoc);
             collectionPlayerpaths.replace(pUUID, pPath);
@@ -69,8 +68,8 @@ public class PlayerpathCommand implements CommandExecutor {
         }
     }
 
-    private void addCoordinateToPolyline(ArrayList<Location> coords, String pName, boolean createNew){
-        if (MonitorMC.INSTANCE.playerPaths != null){
+    private void addCoordinateToPolyline(ArrayList<Location> coords, String pName, boolean createNew) {
+        if (MonitorMC.INSTANCE.playerPaths != null) {
             //konvertiere Locations aus ArrayLists in 3 arrays
             int coordLength = coords.size();
 
@@ -78,19 +77,37 @@ public class PlayerpathCommand implements CommandExecutor {
             double[] coords_y = new double[coordLength];
             double[] coords_z = new double[coordLength];
 
-            for (int i = 0; i < coordLength; i++){
+            for (int i = 0; i < coordLength; i++) {
                 coords_x[i] = coords.get(i).getX();
                 coords_y[i] = coords.get(i).getY();
                 coords_z[i] = coords.get(i).getZ();
             }
 
-            if (createNew){
+            if (createNew) {
                 MonitorMC.INSTANCE.playerPaths.createPolyLineMarker(pName, "just a test label", true, "world", coords_x, coords_y, coords_z, false);
-            }
-            else{
+            } else {
                 MonitorMC.INSTANCE.playerPaths.findPolyLineMarker(pName).setCornerLocations(coords_x, coords_y, coords_z);
             }
 
         }
+    }
+
+    @Override
+    public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
+        List<String> completions = new ArrayList<>();
+//        Map<String, Icon> enumMap = new HashMap<>();
+        if (!(sender instanceof Player)) {
+            return completions;
+        }
+
+        Player player = (Player) sender;
+
+        if (args.length == 1) {
+            completions.add("<name>");
+//            completions.add("help");
+
+            return completions;
+        }
+        return completions;
     }
 }
