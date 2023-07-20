b5b97910936d21e3fb1e114af0b407167cddf23e
Micha Schmitt <38440532+CodedInMyHead@users.noreply.github.com>
2023-07-20 14:55:37 +0200

    easy (#18)
    easy

--- src/main/java/com/codedinmyhead/monitormc/monitormc/listeners/common/ActivatedListeners.java
+++ src/main/java/com/codedinmyhead/monitormc/monitormc/listeners/common/ActivatedListeners.java
@@ -1,17 +1,16 @@
 package com.codedinmyhead.monitormc.monitormc.listeners.common;
 
 import com.codedinmyhead.monitormc.monitormc.gui.DashboardGUI;
-import com.codedinmyhead.monitormc.monitormc.listeners.ArrowHitListener;
-import com.codedinmyhead.monitormc.monitormc.listeners.LeaderboardGUIListener;
-import com.codedinmyhead.monitormc.monitormc.listeners.PlayerJoinListener;
-import com.codedinmyhead.monitormc.monitormc.listeners.SleepListener;
+import com.codedinmyhead.monitormc.monitormc.listeners.*;
 
 public enum ActivatedListeners {
     ARROW_HIT(ArrowHitListener.class),
     Player_JOIN(PlayerJoinListener.class),
     SLEEP_LISTENER(SleepListener.class),
     DASHBOARDGUI_LISTENER(DashboardGUI.class),
-    LEADERBOARD_GUI(LeaderboardGUIListener.class);
+    LEADERBOARD_GUI(LeaderboardGUIListener.class),
+    CHAT_NUMBER_SUM(ChatNumberListener.class),
+    DIAMONDS_MINED(DiamondOreMined.class);
 
     private final Class<?> classType;
 
